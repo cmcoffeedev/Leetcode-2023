035a1b9e674843087b3cf150a7179ce4a7924ad6
Christopher Coffee <chrisc1891@gmail.com>
2023-10-07 23:27:40 -0700

    Time: 195 ms, Memory: 38.3 MB - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        
+        var max = 0
+        
+        for(i in 1 until prices.size){
+            if(prices[i] > prices[i-1]){
+                max += prices[i] - prices[i-1]
+            }
+        }
+        
+        return max
+        
+        
+    }
+}
